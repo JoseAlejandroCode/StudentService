90de06add562217f1b71cd8b64d412a2a5853799
Jose Alejandro <paredes_191@hotmail.com>
2019-11-22 17:06:42 -0500

    change name for service config

--- src/main/java/com/microservice/student/configuration/FamilyServiceConfig.java
+++ src/main/java/com/microservice/student/configuration/FamilyServiceConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class ServiceConfig {
+public class FamilyServiceConfig {
 
   @Value("${config.base.family.uri}")
   private String pathFamily;
